582ba664d6734b8f9b6ad8d5d156b66c1df1c462
bruno <brunocordeiro180@gmail.com>
2018-08-24 10:48:38 -0300

    homepage da evl adicionada

--- evl/views.py
+++ evl/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+# -*- coding: utf-8 -*-
 
-# Create your views here.
+from django.shortcuts import render, redirect,get_object_or_404
+import xlsxwriter
+from django.http import HttpResponse
+from django.http import JsonResponse
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+
+def home(request):
+    return render(request, 'evl/home.html')
