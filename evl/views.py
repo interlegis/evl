70bf83412abd370ec8ba4a133ca54fd5bb2b9990
bruno <brunocordeiro180@gmail.com>
2018-09-17 17:06:18 -0300

    comecando a pegar dados da api

--- evl/views.py
+++ evl/views.py
@@ -2,6 +2,8 @@
 
 from django.shortcuts import render, redirect,get_object_or_404
 import xlsxwriter
+import requests
+import json
 from django.http import HttpResponse
 from django.http import JsonResponse
 from .models import *
@@ -12,8 +14,19 @@ def home(request):
     return render(request, 'evl/home.html')
 
 def cursos(request):
-    cursos = Courses.objects.all()
-    categorias = CourseCategories.objects.all()
+    response_cursos = requests.get('https://escolamodelows.interlegis.leg.br/api/v1/cursos')
+    response_categorias = requests.get('https://escolamodelows.interlegis.leg.br/api/v1/categorias_cursos')
+
+    # print(cursos['cursos'][0][0])
+
+    cursos = response_cursos.json()
+    cursos = json.loads(json.dumps(cursos))
+    cursos = cursos['cursos']
+    
+    categorias = response_categorias.json()
+    categorias = json.loads(json.dumps(categorias))
+    categorias = categorias['categorias_cursos']
+
     return render(request, 'evl/cursos.html', {'cursos' : cursos, 'categorias' : categorias})
 
 def login(request):
