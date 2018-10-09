f319d7824db8775c12a3ee7c70bd0b3ae0b20401
Thiago Urias <thiagourias_@hotmail.com>
2018-10-09 16:52:10 -0300

    Alterada referencia de scripts e identacao

---
+++ static/js/dataTable.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+    $('#minhaTabela').DataTable({
+        "language": {
+            "lengthMenu": " ",
+            "zeroRecords": "Nada encontrado",
+            "info": "Mostrando página _PAGE_ de _PAGES_",
+            "infoEmpty": "Nenhum registro disponível",
+            "loadingRecords": "Carregando...",
+            "processing":     "Processando...",
+            "search":         "Pesquisar:",
+            "infoFiltered": "(filtrado de _MAX_ registros no total)",
+            "paginate": {
+                "first":      "Primeira",
+                "last":       "Última",
+                "next":       "Próxima",
+                "previous":   "Anterior"
+            },
+        }
+    });
+});
