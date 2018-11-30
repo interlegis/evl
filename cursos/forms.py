1e17f5fb784d4a6e19e078b19a45b2314394bf7b
Matheus456 <matheus456456@gmail.com>
2018-11-30 13:39:53 -0200

    Adicionando dados que faltavam para o forms

--- cursos/forms.py
+++ cursos/forms.py
@@ -1,5 +1,17 @@
 # -*- coding: utf-8 -*-
 from django import forms
+from urllib.request import urlopen
+import json
+
+requisicao_categoria = urlopen("https://escolamodelows.interlegis.leg.br/api/v1/categorias_cursos")
+response_categorias = json.load(requisicao_categoria)
+categorias = response_categorias['categorias_cursos']
+lista_categorias = [('-1', '-- Selecione uma categoria --')]
+for categoria in categorias:
+    if categoria['id'] != None:
+        lista_categorias.append((categoria['id'], categoria['nome']))
+
+lista_status = (('', '-- Selecione um estado --'), ('Aprovado', 'Aprovado'), ('Reprovado', 'Reprovado'))
 
 class AvaliarCursosForm(forms.Form):
     status = forms.ChoiceField(
