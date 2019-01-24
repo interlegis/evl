3e2b370df8c0014968b9ea6dc8ccf76f5d9e2700
Thiago Urias Hirae <thiagourias_@hotmail.com>
2019-01-24 15:26:33 -0200

    Catalogo de cursos

--- cursos/static/js/cursos.js
+++ cursos/static/js/cursos.js
@@ -2,6 +2,7 @@ $(document).ready(function() {
   $("#list-tab a").click(function(){
     var selId = $(this).attr('id');
     var id = selId.substring(2,10);
+    console.log(id);
     $("#nav-tabContent > *").css("display", "none")
     $("#"+id+"").css("display", "block");
   });
--- evl_admin/settings.py
+++ evl_admin/settings.py
@@ -181,5 +181,5 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
+    os.path.join("static"),
 ]
