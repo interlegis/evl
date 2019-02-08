ae389041405ba0a2d4364cc9e4911afe68386f52
Matheus456 <matheus456456@gmail.com>
2019-02-08 15:36:09 -0200

    Preenchendo campos existentes no fale conosco

--- fale_conosco/forms.py
+++ fale_conosco/forms.py
@@ -7,7 +7,7 @@ import json
 requisicao_categoria = urlopen("http://localhost:3000/api/v1/categorias_cursos?key=k4B5YcbKa619ohu3wxk2xXbmtoxFuQqrwcKEOTAnZi7iy4tl9z")
 response_categorias = json.load(requisicao_categoria)
 categorias = response_categorias['categorias_cursos']
-lista_categorias = [('-1', '-- Selecione uma categoria --')]
+lista_categorias = [('-1', 'Selecione uma categoria')]
 for categoria in categorias:
     if categoria['id'] != None:
         lista_categorias.append((categoria['id'], categoria['nome']))
@@ -15,7 +15,7 @@ for categoria in categorias:
 requisicao_curso = urlopen("http://localhost:3000/api/v1/cursos?key=k4B5YcbKa619ohu3wxk2xXbmtoxFuQqrwcKEOTAnZi7iy4tl9z")
 response_cursos = json.load(requisicao_curso)
 cursos = response_cursos['cursos']
-lista_cursos = [('-1', '-- Selecione um curso --')]
+lista_cursos = [('-1', 'Selecione um curso')]
 for curso in cursos:
     if curso['id'] != None:
         lista_cursos.append((curso['id'], curso['nome']))
@@ -23,13 +23,13 @@ for curso in cursos:
 requisicao_escola = urlopen("http://localhost:3000/api/v1/escolas?key=k4B5YcbKa619ohu3wxk2xXbmtoxFuQqrwcKEOTAnZi7iy4tl9z")
 response_escolas = json.load(requisicao_escola)
 escolas = response_escolas['escolas']
-lista_escolas = [('-1', '-- Selecione uma escola --')]
+lista_escolas = [('-1', 'Selecione uma escola')]
 for escola in escolas:
     if escola['initials'] != None:
         lista_escolas.append((escola['initials'], escola['nome']))
 
 
-lista_contatos = (('-1', '-- Selecione o tipo de contato --'), ('0', 'Reclamação'), ('1', 'Dúvida'), ('2', 'Sugestão'), ('3', 'Elogio'))
+lista_contatos = (('-1', 'Selecione o tipo de contato'), ('0', 'Reclamação'), ('1', 'Dúvida'), ('2', 'Sugestão'), ('3', 'Elogio'))
 
 class FaleConoscoForm(forms.Form):
     name = forms.CharField(
@@ -80,7 +80,7 @@ class FaleConoscoForm(forms.Form):
         choices = lista_cursos,
         widget = forms.Select(
             attrs={
-                'class' : 'form-control',
+                'class' : 'custom-select',
             }
         )
     )
@@ -89,7 +89,7 @@ class FaleConoscoForm(forms.Form):
         choices = lista_escolas,
         widget = forms.Select(
             attrs={
-                'class' : 'form-control',
+                'class' : 'custom-select',
             }
         )
     )
@@ -97,7 +97,7 @@ class FaleConoscoForm(forms.Form):
         choices = lista_contatos,
         widget = forms.Select(
             attrs={
-                'class' : 'form-control',
+                'class' : 'custom-select',
             }
         )
     )
@@ -105,7 +105,7 @@ class FaleConoscoForm(forms.Form):
         choices = lista_categorias,
         widget = forms.Select(
             attrs={
-                'class' : 'form-control',
+                'class' : 'custom-select',
             }
         )
     )
@@ -117,7 +117,7 @@ class FaleConoscoForm(forms.Form):
                 'placeholder' : 'Mensagem',
                 'rows': '10',
                 'cols': '50',
-                'style': 'max-height: 400px;',
+                'style': 'max-height: 400px; min-height: 50px',
             }
         )
     )
@@ -133,6 +133,123 @@ class FaleConoscoForm(forms.Form):
 
     class Meta:
         fields = ('name', 'email', 'cpf', 'course_id', 'type_conversation', 'course_category_id', 'title', 'description', 'school_initials')
+
+    def clean_school_initials(self, *args, **kwargs):
+        school_initials = self.cleaned_data.get("school_initials")
+        print("INICIAIS = ", school_initials)
+        if "-1" in school_initials:
+            raise forms.ValidationError("Este campo é obrigatório! Selecione uma escola.")
+        else:
+            return school_initials
+
+    def clean_type_conversation(self, *args, **kwargs):
+        type_conversation = self.cleaned_data.get("type_conversation")
+        if "-1" in type_conversation:
+            raise forms.ValidationError("Este campo é obrigatório! Selecione um tipo de contato.")
+        else:
+            return type_conversation
+
+class FaleConoscoFormLogged(forms.Form):
+    name = forms.CharField(
+        required = False,
+        widget=forms.TextInput(
+            attrs={
+                'class' : 'form-control',
+                'placeholder' : 'Nome',
+            }
+        )
+    )
+
+    email = forms.EmailField(
+        required = False,
+        widget=forms.TextInput(
+            attrs={
+                'class' : 'form-control',
+                'placeholder' : 'Email',
+            }
+        )
+    )
+    cpf = BRCPFField(
+        required = False,
+        widget=forms.TextInput(
+            attrs={
+                'class': 'form-control',
+                'placeholder': 'CPF',
+
+            }
+        )
+    )
+    forms.CharField(widget=forms.TextInput(
+        attrs={
+            'class' : 'form-control',
+            'placeholder' : 'CPF',
+        }
+    ))
+    phone_number = forms.CharField(
+        required = False,
+        widget=forms.TextInput(
+            attrs={
+                'class' : 'form-control',
+                'placeholder' : 'Numero Celular',
+            }
+    ))
+    course_id = forms.ChoiceField(
+        choices = lista_cursos,
+        widget = forms.Select(
+            attrs={
+                'class' : 'custom-select',
+            }
+        )
+    )
+    school_initials = forms.ChoiceField(
+        error_messages={'required': 'Este campo é obrigatório! Escolha a sua escola.'},
+        choices = lista_escolas,
+        widget = forms.Select(
+            attrs={
+                'class' : 'custom-select',
+            }
+        )
+    )
+    type_conversation = forms.ChoiceField(
+        choices = lista_contatos,
+        widget = forms.Select(
+            attrs={
+                'class' : 'custom-select',
+            }
+        )
+    )
+    course_category_id = forms.ChoiceField(
+        choices = lista_categorias,
+        widget = forms.Select(
+            attrs={
+                'class' : 'custom-select',
+            }
+        )
+    )
+    description = forms.CharField(
+        error_messages={'required': 'Este campo é obrigatório! Preencha este campo com a sua mensagem.'},
+        widget=forms.Textarea(
+            attrs={
+                'class' : 'form-control',
+                'placeholder' : 'Mensagem',
+                'rows': '10',
+                'cols': '50',
+                'style': 'max-height: 400px; min-height: 50px',
+            }
+        )
+    )
+    title = forms.CharField(
+        error_messages={'required': 'Este campo é obrigatório! Preencha este campo com o título.'},
+        widget=forms.TextInput(
+            attrs={
+                'class' : 'form-control',
+                'placeholder' : 'Título',
+            }
+        )
+    )
+
+    class Meta:
+        fields = ('course_id', 'type_conversation', 'course_category_id', 'title', 'description', 'school_initials')
 
     def clean_school_initials(self, *args, **kwargs):
         school_initials = self.cleaned_data.get("school_initials")
--- fale_conosco/views.py
+++ fale_conosco/views.py
@@ -7,7 +7,7 @@ import requests
 import json
 from .models import *
 from django.shortcuts import render_to_response
-from .forms import FaleConoscoForm
+from .forms import FaleConoscoForm, FaleConoscoFormLogged
 from urllib.request import urlopen
 import urllib.parse
 import urllib.request
@@ -17,9 +17,22 @@ from django.http.response import HttpResponse
 #@login_required(login_url='http://localhost:3000/log_in?return=XXX')
 def faleConosco(request):
     if request.method == "POST":
+        formLogged = FaleConoscoFormLogged(request.POST)
         form = FaleConoscoForm(request.POST)
+        if request.user.profile and formLogged.is_valid():
+            formLogged.fields['name'] = request.user.username
+            formLogged.fields['email'] = request.user.email
+            formLogged.fields['cpf'] = request.user.profile.cpf
+            formLogged.fields['phone_number'] = request.user.profile.phone
+            req = urllib.request.Request('http://localhost:3000/api/v1/fale_conosco/adicionar?name=' + request.user.username + "&email="+ request.user.email + "&cpf=" + request.user.profile.cpf )
+            req.add_header('Content-Type', 'application/json; charset=utf-8')
+            result = urlopen(req, json.dumps(formLogged.data).encode('utf-8'))
+            return render(request, 'evl/home.html', context={'messagem_faleConosco': "A mensagem foi enviada com sucesso"})
+        else:
+            print("ERROS =", form.errors)
+            return render(request, 'faleConosco.html', {'form': formLogged})
+
         if form.is_valid():
-            # req = urllib.request.Request('http://localhost:3000/api/v1/fale_conosco/adicionar')
             req = urllib.request.Request('http://localhost:3000/api/v1/fale_conosco/adicionar')
             req.add_header('Content-Type', 'application/json; charset=utf-8')
             result = urlopen(req, json.dumps(form.data).encode('utf-8'))
@@ -34,13 +47,10 @@ def faleConosco(request):
 def mensagensFaleConosco(request):
     if request.method == "POST":
         id = request.POST['id']
-        # req = requests.post('http://localhost:3000/api/v1/fale_conosco/mensagens?conversation_id=' + id)
         req = requests.post('http://localhost:3000/api/v1/fale_conosco/mensagens?conversation_id=' + id)
-        # data = json.dumps(req.content.decode(encoding="utf-8"))
         data = req.content.decode(encoding="utf-8")
         return HttpResponse(data, content_type='application/json')
     else:
-        # req = requests.post('http://localhost:3000/api/v1/fale_conosco/conversa_usuario?cpf=045.232.691-57')
         req = requests.post('http://localhost:3000/api/v1/fale_conosco/conversa_usuario?cpf=045.232.691-57')
         messages = json.loads(req.content)
         return render(request, 'mensagensFaleConosco.html', {'messages': messages})
