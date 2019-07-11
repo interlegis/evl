061a9c51bf17bbc7cd11dec30eb2e25cbe3bae0b
Matheus456 <matheus456456@gmail.com>
2019-07-11 17:08:46 -0300

    Consertando bug da imagem do perfil

--- evl/login.py
+++ evl/login.py
@@ -21,7 +21,7 @@ class MyOIDCAB(OIDCAuthenticationBackend):
         user.profile.phone=claims.get('phone', '')
         user.profile.key=claims.get('access_key', '')
         user.profile.role=claims.get('role','')
-        user.profile.image=claims.get('profile_image', '')
+        user.profile.image=settings.BASE_URL + claims.get('profile_image', '')
         user.profile.save()
 
         return user
