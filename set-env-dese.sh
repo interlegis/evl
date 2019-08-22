fdcec9b4ac3cdad2b08947532051509f73b6ee28
Matheus Garcia <garcia.figueiredo@gmail.com>
2019-08-22 13:33:38 -0300

    Arquivos para definir variáveis de ambiente

---
+++ set-env-dese.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+#Script para inicialização de variáveis de ambiente
+#necessárias para execução da EVL em ambiente de desenvolvimento
+
+export EVL_DB_NAME="moodlews"
+export EVL_DB_USER="escolamodelows"
+export EVL_DB_PASS="escolamodelo"
+export EVL_DB_HOST="campeirohsabereslb.interlegis.leg.br"
+export EVL_DB_PORT="5432"
+export EVL_TOKEN_APPLICATION_ID="7001568ef6842b2419d42ab4aa3837f757de9530ead1287d4124fc435e14a62b"
+export EVL_TOKEN_SECRET="9c2383777624fff0b271cdbabfefacc6c1ddd2ced9ba559ce1aed25e5d169f09"
+export EVL_WS_URL_BASE="https://escolamodelows.interlegis.leg.br"
+export EVL_WS_APIKEY="k4B5YcbKa619ohu3wxk2xXbmtoxFuQqrwcKEOTAnZi7iy4tl9z"
---
+++ set-env-local.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#Script para inicialização de variáveis de ambiente
+#necessárias para execução da EVL em ambiente de desenvolvimento
+
+export EVL_DB_NAME="moodlews"
+export EVL_DB_USER="escolamodelows"
+export EVL_DB_PASS="escolamodelo"
+export EVL_DB_HOST="campeirohsabereslb.interlegis.leg.br"
+export EVL_DB_PORT="5432"
+export EVL_TOKEN_APPLICATION_ID="9aebdb0fb3dcacbdd19629911ecb11e2bfe8d0ffc1e946375f564b007dcb547e"
+export EVL_TOKEN_SECRET="dbd6a666a18e9ace1eba8e63f9a9b1bc2b6019c5b482c5d0ef8a8dd37de9d0f2"
+export EVL_WS_URL_BASE="http://localhost:3000"
+export EVL_WS_APIKEY="6iwXud8cubFTlGWgd8FucO6kzT8BAFXRfd7eXlZLMT3bOgPuMg"
+export EVL_CENTRAL_URL_BASE="http://localhost:3001"
